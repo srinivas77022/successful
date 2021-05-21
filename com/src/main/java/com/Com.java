c57a3e47c0c61d2f366d795bf884765e5adfa0f3
srini <srini@192.168.0.5>
2021-05-22 00:18:13 +0530

    my frist successful

---
+++ com/src/main/java/com/Com.java
@@ -0,0 +1,23 @@
+package com;
+
+public class Com {
+
+	public static void main(String[] args) {
+	int a=0;
+    int b=1;
+    System.out.println(a);
+    System.out.println(b);
+    for (int c=1;c<=100;c=a+b) {
+    	
+    	System.out.println(c); 
+    	a=b;
+        b=c;
+    	
+    	
+    	
+    	}
+	
+
+	}
+
+}
