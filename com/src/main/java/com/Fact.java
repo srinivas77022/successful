34ab1031f9c4a85e04f05552709cb01d901ac002
srini <srini@192.168.0.5>
2021-05-23 20:16:56 +0530

    successful

---
+++ com/src/main/java/com/Fact.java
@@ -0,0 +1,21 @@
+package com;
+
+public class Fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a=8;
+	int b=a-1;
+	
+	for(int c=a;c<=a*b;c++) {
+	   c=a*b;
+	   System.out.println(c);
+		a=c;
+		
+		b=b-1;
+		
+	}
+	
+	
+	}
+	}
---
+++ com/src/main/java/com/Table.java
@@ -0,0 +1,19 @@
+package com;
+
+public class Table {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=4;
+		int n;
+		for (n=1;n<=10;n++) {
+			
+			int c;
+			c=a*n;
+			System.out.println(a +"*" +n +"="+c); 
+			
+		}
+
+	}
+
+}
